a84ab8c193b93641c125068c567bed7bae0b1256
Richard Rossetto <richard.rossetto@hotmail.com>
2019-10-13 21:07:40 +0100

    Add description challenge

--- src/main/java/com/hackerrank/Challenge/VeryBigSum.java
+++ src/main/java/com/hackerrank/Challenge/VeryBigSum.java
@@ -1,17 +1,37 @@
 package com.hackerrank.Challenge;
 
+
+/*
+ * Calculate and print the sum of the elements in an array, 
+ * keeping in mind that some of those integers may be quite large.
+ * Function Description
+ * Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+ * aVeryBigSum has the following parameter(s):
+ * ar: an array of integers .
+ * Input Format
+ * The first line of the input consists of an integer n.
+ * The next line contains n space-separated integers contained in the array.
+ * Output Format
+ * Print the integer sum of the elements in the array.
+ * Ex.:
+ * Input
+ * 1000000001 1000000002 1000000003 1000000004 1000000005
+ * Output
+ * 5000000015 
+ */
+
 public class VeryBigSum {
 	public static void main(String[] args) {
-		long[] ar = {5, 1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
+		long[] ar = {1000000001, 1000000002, 1000000003, 1000000004, 1000000005};
 		
 		System.out.println(aVeryBigSum(ar));
 	}
 	
 	static long aVeryBigSum(long[] ar) {
-		long count=0;
+		long sum=0;
 		for(int i=0; i < ar.length; i++) {
-			count = count + ar[i];
+			sum = sum + ar[i];
 		}
-		return count;
+		return sum;
     }
 }
